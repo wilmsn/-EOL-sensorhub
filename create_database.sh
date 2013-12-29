cc2ac173597546bcfdebecaca9405de909bd6930
Norbert Wilms <mail@wilmie.de>
2013-12-29 20:52:06 +0100

    V0.3 some small bugfixes

--- create_database.sh
+++ create_database.sh
@@ -7,21 +7,22 @@ if [ -e $DB ]; then
 fi  
 sqlite3 $DB <<EOF
 CREATE TABLE Node
-       (Node TEXT, Battery_UN FLOAT, Battery_UE FLOAT, Location TEXT, Battery_act FLOAT, Nodename TEXT,
-       PRIMARY KEY (Node));
+       (Node TEXT PRIMARY KEY, Battery_UN FLOAT, Battery_UE FLOAT, Location TEXT, Battery_act FLOAT, Nodename TEXT);
 CREATE TABLE job
        ( jobno INT, seq INT, Sensor INT, value FLOAT,
-       PRIMARY KEY ( jobno, seq ));
-CREATE TABLE jobdesc(jobno INT, jobdesc TEXT);
+       PRIMARY KEY ( jobno, seq ),
+       FOREIGN KEY (Sensor) REFERENCES Sensor(Sensor) );
+CREATE TABLE jobchain
+       (jobno INT, jobdesc TEXT);
 CREATE TABLE messagebuffer
        ( jobno INT, seq INT, node TEXT, channel INT, value FLOAT,
        PRIMARY KEY ( jobno, seq ));
-CREATE TABLE schedule 
-       (schedule INT,jobno int, Start, Interval INT,
-       PRIMARY KEY (schedule));
+CREATE TABLE schedule
+       (schedule INT PRIMARY KEY,jobno int, Start, Interval INT,
+       FOREIGN KEY (Jobno) REFERENCES jobchain(JOBNO));
 CREATE TABLE sensor
-       (Sensor INT, Sensorinfo TEXT, Node TEXT, Channel INT, Last_Value FLOAT, Last_TS TEXT,
-       PRIMARY KEY (Sensor));
+       (Sensor INT PRIMARY KEY, Sensorinfo TEXT, Node TEXT, Channel INT, Last_Value FLOAT, Last_TS TEXT,
+       FOREIGN KEY (Node) REFERENCES Node(Node) );
 CREATE TABLE sensordata
        (Sensor INT, Year INT, Month INT, Day INT, Hour INT, Value FLOAT,
        PRIMARY KEY (Year, Month, Day, Hour, Sensor));
@@ -32,7 +33,6 @@ CREATE VIEW job_v as
        select  a.jobno, a.seq, b.node, b.channel, a.value
        from job a, sensor b
        where a.sensor = b.sensor;
-
 EOF
 
 
