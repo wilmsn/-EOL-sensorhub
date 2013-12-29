33496a06cd4ccc738b7c1c639ff3cfd56e075427
Norbert Wilms <mail@wilmie.de>
2013-12-29 15:35:13 +0100

    Version 0.3

--- create_database.sh
+++ create_database.sh
@@ -6,31 +6,32 @@ if [ -e $DB ]; then
   rm $DB
 fi  
 sqlite3 $DB <<EOF
-create table Node 
-       (Node TEXT, Battery_UN FLOAT, Battery_UE FLOAT, Location TEXT, 
+CREATE TABLE Node
+       (Node TEXT, Battery_UN FLOAT, Battery_UE FLOAT, Location TEXT, Battery_act FLOAT, Nodename TEXT,
        PRIMARY KEY (Node));
-
-create table sensor 
+CREATE TABLE job
+       ( jobno INT, seq INT, Sensor INT, value FLOAT,
+       PRIMARY KEY ( jobno, seq ));
+CREATE TABLE jobdesc(jobno INT, jobdesc TEXT);
+CREATE TABLE messagebuffer
+       ( jobno INT, seq INT, node TEXT, channel INT, value FLOAT,
+       PRIMARY KEY ( jobno, seq ));
+CREATE TABLE schedule 
+       (schedule INT,jobno int, Start, Interval INT,
+       PRIMARY KEY (schedule));
+CREATE TABLE sensor
        (Sensor INT, Sensorinfo TEXT, Node TEXT, Channel INT, Last_Value FLOAT, Last_TS TEXT,
        PRIMARY KEY (Sensor));
-
-create table sensordata 
+CREATE TABLE sensordata
        (Sensor INT, Year INT, Month INT, Day INT, Hour INT, Value FLOAT,
        PRIMARY KEY (Year, Month, Day, Hour, Sensor));
-
-CREATE TABLE job 
-       ( jobno INT, seq INT, node TEXT, channel INT, value FLOAT,  
-       PRIMARY KEY ( jobno, seq ));
-
-CREATE TABLE schedule (jobno int, Start, Nextstart INT, primary key (jobno));
-
-CREATE TABLE messagebuffer 
-       ( jobno INT, seq INT, node TEXT, channel INT, value FLOAT,  
-       PRIMARY KEY ( jobno, seq ));
-
 CREATE VIEW message2send
-       as select jobno, min(seq) as aseq, node, channel, value from messagebuffer 
+       as select jobno, min(seq) as aseq, node, channel, value from messagebuffer
        group by jobno;
+CREATE VIEW job_v as
+       select  a.jobno, a.seq, b.node, b.channel, a.value
+       from job a, sensor b
+       where a.sensor = b.sensor;
 
 EOF
 
