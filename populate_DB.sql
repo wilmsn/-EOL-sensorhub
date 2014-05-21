1e230be03d4f08355da564596c906833dcf84738
Norbert Wilms <mail@wilmie.de>
2014-05-21 11:15:13 +0200

    cleanup 21.05.2014

--- populate_DB.sql
+++ populate_DB.sql
@@ -14,31 +14,14 @@ CREATE TABLE "actordata"
  (Actor INT, utime INT default ( strftime('%s','now')),Value float, PRIMARY KEY (Actor));
 CREATE TABLE job
        (Job INT, Seq INT, type INT, ID INT, PRIMARY KEY ( job, seq ));
-CREATE TABLE job1(
-  "job+1000",
-  Seq INT,
-  type INT,
-  ID INT
-);
 CREATE TABLE jobchain(job INT,jobdesc TEXT);
 CREATE TABLE messagebuffer
        ( job INT, seq INT, node TEXT, channel INT, value FLOAT, utime INT default ( strftime('%s','now')), priority INT default 9, FOREIGN KEY (Job) REFERENCES jobchain(JOB));
-CREATE TABLE messagebuffer_test(
-  job INT,
-  seq INT,
-  node TEXT,
-  channel INT,
-  value REAL,
-  utime INT
-);
 CREATE TABLE schedule
        (schedule INT PRIMARY KEY, job int, Start, Interval INT, type INT, trigger INT,
        FOREIGN KEY (Job) REFERENCES jobchain(JOB));
 CREATE TABLE "sensordata"
        (Sensor INT, utime int, Value float, PRIMARY KEY (utime, sensor));
-CREATE TABLE "sensordata_old"
-       (Sensor INT, Year INT, Month INT, Day INT, Hour INT, Value FLOAT,
-       PRIMARY KEY (Year, Month, Day, Hour, Sensor));
 CREATE VIEW Scheduled_messages as
        SELECT a.job, a.seq, b.node, b.channel, 0 as value from job a, sensor b
          where  a.type = 1 and a.id = b.sensor and a.job in ( select job from Scheduled_Jobs )
@@ -54,41 +37,9 @@ CREATE VIEW sensors_and_actors as
        SELECT  's'||sensor as key, 1 as type, sensor as id, Sensorinfo as info, node, channel from Sensor
        union all
        SELECT 'a'||actor , 2, actor, Actorinfo, node, channel from Actor;
-CREATE VIEW message2send
-       as select job, min(seq) as aseq, node, channel, value from messagebuffer
-where strftime('%s','now') - utime < 600
-group by job;
-CREATE VIEW sensordata1 as
-SELECT strftime('%Y',datetime(utime, 'unixepoch')) as year,
-       strftime('%m',datetime(utime, 'unixepoch')) as month,
-       strftime('%d',datetime(utime, 'unixepoch')) as day,
-       strftime('%H',datetime(utime, 'unixepoch')) as hour,
-       strftime('%M',datetime(utime, 'unixepoch')) as minute,
-       nodename, sensorinfo, value, a.sensor, utime
- from sensordata a, sensor b, node c
-where a.sensor = b.sensor and b.node = c.node
-order by utime asc;
 CREATE VIEW all_sensors as
 select sensor, sensorinfo, nodename, last_value, last_ts
  from sensor b, node c
 where b.node = c.node
 order by nodename;
-CREATE VIEW sensordata2 as
-SELECT datetime(utime, 'unixepoch') as timestamp,
-       nodename, sensorinfo, value, a.sensor, utime
- from sensordata a, sensor b, node c
-where a.sensor = b.sensor and b.node = c.node
-order by utime asc;
-CREATE VIEW message2send_n1 as
-select  job, seq as aseq, node, channel, value from  messagebuffer where substr(Node,length(node),1) = '1' order by CAST(node as integer);
-CREATE VIEW message2send_n2 as
-select  job, seq as aseq, node, channel, value from  messagebuffer where substr(Node,length(node),1) = '2' order by CAST(node as integer);
-CREATE VIEW message2send_n3 as
-select  job, seq as aseq, node, channel, value from  messagebuffer where substr(Node,length(node),1) = '3' order by CAST(node as integer);
-CREATE VIEW message2send_n4 as
-select  job, seq as aseq, node, channel, value from  messagebuffer where substr(Node,length(node),1) = '4' order by CAST(node as integer);
-CREATE VIEW message2send_n5 as
-select  job, seq as aseq, node, channel, value from  messagebuffer where substr(Node,length(node),1) = '5' order by CAST(node as integer);
-CREATE VIEW message2send_n6 as
-select  job, seq as aseq, node, channel, value from  messagebuffer where substr(Node,length(node),1) = '6' order by CAST(node as integer);
 
