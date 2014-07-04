c54a7dfbb0560e9281100c161efa7b0e1ecda5b9
Norbert Wilms <mail@wilmie.de>
2014-07-04 19:15:45 +0200

    V0.6 Triggers added

--- create_database.sh
+++ create_database.sh
@@ -2,6 +2,14 @@
 
 DB=sensorhub.db
 
+#
+# This is only for development
+# Do not create this file in Production !!!!!!
+#
+if [ -f /home/norbert/entw/sensorhub/this_is_in_development ]; then
+  echo ".schema" | sqlite3 /var/database/sensorhub.db > populate_DB.sql
+fi
+
 if [ -e $DB ]; then
   rm $DB
 fi  
