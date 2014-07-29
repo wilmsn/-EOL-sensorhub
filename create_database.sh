559cf43babab790095db109ef97c18609495a274
Norbert Wilms <mail@wilmie.de>
2014-07-29 17:54:00 +0200

    Version from 29.07.14

--- create_database.sh
+++ create_database.sh
@@ -7,14 +7,16 @@ DB=sensorhub.db
 # Do not create this file in Production !!!!!!
 #
 if [ -f /home/norbert/entw/sensorhub/this_is_in_development ]; then
-  echo ".schema" | sqlite3 /var/database/sensorhub.db > populate_DB.sql
+  echo ".schema" | sqlite3 /var/database/$DB > create_DB.sql
 fi
 
 if [ -e $DB ]; then
   rm $DB
 fi  
 
+cat create_DB.sql | sqlite3 $DB
+
 if [ -e populate_DB.sql ]; then
-  sqlite3 $DB < populate_DB.sql
+  cat populate_DB.sql | sqlite3 $DB 
   echo  "Database: $DB filled with data from populate_DB.sql"
 fi
